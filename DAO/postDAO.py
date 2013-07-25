8d0dfd0d75b2642852df6447bb7c7369613a512b
Raghotham S <raghotham.s@sap.com>
2013-07-25 23:54:48 +0530

    adding Try blocks for db reads and writes

--- DAO/postDAO.py
+++ DAO/postDAO.py
@@ -2,6 +2,7 @@ __author__ = 'raghothams'
 
 from model.post import Post
 import datetime
+import bson.ObjectId
 
 class PostDAO:
 
@@ -12,12 +13,23 @@ class PostDAO:
 
 	def get_recent_posts(self, groupid):
 		
-		collection = self.recent_collection
-		posts = collection.find({
-			'group' : groupid
-			})
-		post_list = self.get_modelled_list(posts)
-		return post_list
+		groupid = ObjectId(groupid)
+		posts = None
+		try:
+			collection = self.recent_collection
+			posts = collection.find({
+				'group' : groupid
+				})
+
+		except Exception as inst:
+			print "error reading recent posts"
+			print inst
+
+		if posts != None:
+			post_list = self.get_modelled_list(posts)
+			return post_list
+		else:
+			return None
 
 	def get_archived_posts(self, groupid):
 
@@ -34,29 +46,36 @@ class PostDAO:
 
 		modelled_post_list = []
 		for post in posts:
-
-			modelled_post = Post()
-			modelled_post.id = post['_id']
-			print post['date']
-			date_obj = post['date']
-			modelled_post.date = date_obj.isoformat()
-			modelled_post.title = post['title']
-			modelled_post.link = post['link']
-			modelled_post.category = post['category']
-			modelled_post.tags = post['tags']
-			modelled_post.group = post['group']
-			modelled_post.added_by = post['added_by']
+			try:
+				modelled_post = Post()
+				modelled_post.id = post['_id']
+				# print post['date']
+				date_obj = post['date']
+				modelled_post.date = date_obj.isoformat()
+				modelled_post.title = post['title']
+				modelled_post.link = post['link']
+				modelled_post.category = post['category']
+				modelled_post.tags = post['tags']
+				modelled_post.group = post['group']
+				modelled_post.added_by = post['added_by']
 			
-			modelled_post_list.append(modelled_post)
+				modelled_post_list.append(modelled_post)
+			except Exception as inst:
+				print "error processing objects"
+				print inst
 
 		return modelled_post_list
 
 	def insert_post(self, post_obj):
 
 		collection = self.recent_collection
-
-		to_insert = post_obj.db_serializer()
-		print to_insert
-		result = collection.insert(to_insert)
+		result = None
+		try:
+			to_insert = post_obj.db_serializer()
+			# print to_insert
+			result = collection.insert(to_insert)
+		except Exception as inst:
+			print "error writing post"
+			print inst
 
 		return result
